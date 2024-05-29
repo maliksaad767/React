6a9bd7e59e46fcf86998e0acd5b29fb925eafc87
maliksaad767 <78859464+maliksaad767@users.noreply.github.com>
2024-05-29 06:43:37 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'; old code
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
-  return (
-    <>
-    
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TaxtUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-        
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
 
-    </>
+  const [mode, setMode] = useState('light');
+  const togglemode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey';
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  };
+
+  return (
+    <Router>
+      <Navbar title='TextUtils' About='About Us' mode={mode} togglemode={togglemode} />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/" element={<TextForm heading='Enter the text to analyze below' mode={mode} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
